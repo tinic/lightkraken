d2e26094ac9e8ee79a0a30591b3d954e23b46a39
Tinic Uro <turo@adobe.com>
2019-11-07 14:22:27 -0800

    Record min and max times.

--- perf.cpp
+++ perf.cpp
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <memory.h>
+#include <algorithm>
 
 #include "./systick.h"
 #include "./perf.h"
@@ -23,20 +24,30 @@ PerfMeasure::PerfMeasure(Slot slot_) {
 	if (!init) {
 		init = true;
 		memset(slots, 0, sizeof(slots));
+		for (int32_t c = 0; c < SLOT_COUNT ; c++) {
+			slots[c].min = ~uint64_t(0);
+		}
 	}
 	slot = slot_;
 	start_time = Systick::instance().systemTick();
 }
 
 PerfMeasure::~PerfMeasure() {
-	slots[slot].accumulated += Systick::instance().systemTick() - start_time;
+	uint64_t this_interval = Systick::instance().systemTick() - start_time;
+
+	slots[slot].accumulated += this_interval;
 	slots[slot].count ++;
+
+	slots[slot].max = std::max(slots[slot].max, this_interval);
+	slots[slot].min = std::min(slots[slot].min, this_interval);
 }
 
 void PerfMeasure::print() {
 	for (int32_t c = 0; c < SLOT_COUNT ; c++) {
 		uint64_t avg_time = slots[c].accumulated / slots[c].count;
-		printf("%s: %d\n", slotNames[c], int(avg_time));
+		uint64_t min_time = (slots[c].min == (~uint64_t(0))) ? 0 : slots[c].max;
+		uint64_t max_time = slots[c].max;
+		printf("%s: avg(%012d) min(%012d) max(%012d)\n", slotNames[c], int(avg_time), int(min_time), int(max_time));
 	}
 }
 
--- perf.h
+++ perf.h
@@ -24,6 +24,8 @@ private:
 
 	struct Entry {
 		uint64_t accumulated;
+		uint64_t max;
+		uint64_t min;
 		uint64_t count;
 	};
 
