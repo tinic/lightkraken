885c323939ffc59913c566f7a57a8253de884034
Tinic Uro <turo@adobe.com>
2019-11-15 10:55:30 -0800

    Use toolchain file.

--- build_release_all.sh
+++ build_release_all.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=0 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=0 ..
 make -j
-cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=1 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=1 ..
 make -j
-cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=1 -DBOOTLOADED=0 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=1 -DBOOTLOADED=0 ..
 make -j
