37526eef60e397eace8b3021eba1435973a62fed
Tinic Uro <turo@adobe.com>
2019-11-15 12:56:16 -0800

    More project cleanup.

--- build_release_all.sh
+++ build_release_all.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 mkdir -p build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=0 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=0 -DBOOTLOADED=1 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADED=1 ..
 make -j
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=1 -DBOOTLOADED=0 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=1 ..
 make -j
