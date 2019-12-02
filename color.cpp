04b43eecf491571d72de18a5124601f016843bda
Tinic Uro <turo@adobe.com>
2019-12-02 15:34:06 -0800

    Inline color conversion.

--- color.cpp
+++ color.cpp
@@ -45,59 +45,6 @@ void CIETransferfromsRGBTransferLookup::init() {
     }
 }
 
-static constexpr int32_t fixed_shift = 24;
-static constexpr int32_t fixed_post_shift = 8;
-static constexpr int32_t fixed_post_clamp = 1UL << (fixed_shift - fixed_post_shift);
-
-__attribute__((used))
-static int32_t mul_fixed(int32_t x, int32_t y) {
-    return int32_t((int64_t(x) * int64_t(y)) >> fixed_shift);
-}
-
-void ColorSpaceConverter::sRGB8toLEDPWM(
-        uint8_t srgb_r, 
-        uint8_t srgb_g, 
-        uint8_t srgb_b,
-        uint16_t pwm_l,
-        uint16_t &pwm_r,
-        uint16_t &pwm_g,
-        uint16_t &pwm_b) const {
-#if 1
-    int32_t lr = srgb_2_srgbl_lookup_fixed[srgb_r];
-    int32_t lg = srgb_2_srgbl_lookup_fixed[srgb_g];
-    int32_t lb = srgb_2_srgbl_lookup_fixed[srgb_b];
-    
-    int32_t x = mul_fixed(srgbl2ledl_fixed[0], lr) + mul_fixed(srgbl2ledl_fixed[1], lg) + mul_fixed(srgbl2ledl_fixed[2], lb);
-    int32_t y = mul_fixed(srgbl2ledl_fixed[3], lr) + mul_fixed(srgbl2ledl_fixed[4], lg) + mul_fixed(srgbl2ledl_fixed[5], lb);
-    int32_t z = mul_fixed(srgbl2ledl_fixed[6], lr) + mul_fixed(srgbl2ledl_fixed[7], lg) + mul_fixed(srgbl2ledl_fixed[8], lb);
-
-    pwm_r = uint16_t((std::clamp((x >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
-    pwm_g = uint16_t((std::clamp((y >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
-    pwm_b = uint16_t((std::clamp((z >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
-#else            
-    float col[3];
-    col[0] = float(srgb_r) * (1.0f / 255.0f);
-    col[1] = float(srgb_g) * (1.0f / 255.0f);
-    col[2] = float(srgb_b) * (1.0f / 255.0f);
-    
-    sRGB2sRGBL(col, col); // sRGB to Linear sRGB
-    sRGBL2LEDL(col, col); // Linear sRGB -> XYZ -> Linear LED RGB
-    
-    for (size_t c = 0; c < 3; c++) {
-        if (col[c] < 0.0f) {
-            col[c] = 0.0f;
-        }
-        if (col[c] > 1.0f) {
-            col[c] = 1.0f;
-        }
-    }
-    
-    pwm_r = uint16_t(col[0]*float(pwm_l));
-    pwm_g = uint16_t(col[1]*float(pwm_l));
-    pwm_b = uint16_t(col[2]*float(pwm_l));
-#endif            
-}
-
 void ColorSpaceConverter::setRGBColorSpace(const RGBColorSpace &rgbSpace) {
     // Generate XYZ to LED matrices
     generateRGBMatrix(
--- color.h
+++ color.h
@@ -25,8 +25,11 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdint.h>
 #include <string.h>
+#include <algorithm>
 
 namespace lightkraken {
+
+
 
 //#define TEST_SRGB_IDENTITY
 
@@ -40,14 +43,14 @@ struct RGBColorSpace {
     }
 
     void setWorldSemiLED() {
-        xw = 0.29902f; yw = 0.31485f; // D75 white point
-        xr = 0.64000f; yr = 0.33000f;
-        xg = 0.30000f; yg = 0.60000f;
-        xb = 0.15000f; yb = 0.06000f;
+        xw = 0.31271f; yw = 0.32902f; // D65 white point
+        xr = 0.68930f; yr = 0.30740f; // from world-semi.com
+        xg = 0.13940f; yg = 0.72140f;
+        xb = 0.13180f; yb = 0.07200f;
     }
 
     void setCreeLED() {
-        xw = 0.31271f; yw = 0.32902f; // D65 white point
+        xw = 0.35000f; yw = 0.33000f; // D65 white point
         xr = 0.67630f; yr = 0.32370f; // from http://ww1.microchip.com/downloads/en/AppNotes/00001562B.pdf
         xg = 0.20880f; yg = 0.74070f;
         xb = 0.14050f; yb = 0.03910f;
@@ -67,18 +70,62 @@ public:
 
 class ColorSpaceConverter {
 public:
-    void sRGB8toLEDPWM(
-        uint8_t srgb_r,
-        uint8_t srgb_g,
-        uint8_t srgb_b,
-        uint16_t pwm_l,
-        uint16_t &pwm_r,
-        uint16_t &pwm_g,
-        uint16_t &pwm_b) const;
-    
+
+	static constexpr int32_t fixed_shift = 24;
+	static constexpr int32_t fixed_post_shift = 8;
+	static constexpr int32_t fixed_post_clamp = 1UL << (fixed_shift - fixed_post_shift);
+
     void setRGBColorSpace(const RGBColorSpace &rgbSpace);
 
+	inline void sRGB8toLEDPWM(
+			uint8_t srgb_r, 
+			uint8_t srgb_g, 
+			uint8_t srgb_b,
+			uint16_t pwm_l,
+			uint16_t &pwm_r,
+			uint16_t &pwm_g,
+			uint16_t &pwm_b) const {
+#if 1
+		int32_t lr = srgb_2_srgbl_lookup_fixed[srgb_r];
+		int32_t lg = srgb_2_srgbl_lookup_fixed[srgb_g];
+		int32_t lb = srgb_2_srgbl_lookup_fixed[srgb_b];
+	
+		int32_t x = mul_fixed(srgbl2ledl_fixed[0], lr) + mul_fixed(srgbl2ledl_fixed[1], lg) + mul_fixed(srgbl2ledl_fixed[2], lb);
+		int32_t y = mul_fixed(srgbl2ledl_fixed[3], lr) + mul_fixed(srgbl2ledl_fixed[4], lg) + mul_fixed(srgbl2ledl_fixed[5], lb);
+		int32_t z = mul_fixed(srgbl2ledl_fixed[6], lr) + mul_fixed(srgbl2ledl_fixed[7], lg) + mul_fixed(srgbl2ledl_fixed[8], lb);
+
+		pwm_r = uint16_t((std::clamp((x >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
+		pwm_g = uint16_t((std::clamp((y >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
+		pwm_b = uint16_t((std::clamp((z >> fixed_post_shift), int32_t(0), fixed_post_clamp) * pwm_l) / fixed_post_clamp);
+#else            
+		float col[3];
+		col[0] = float(srgb_r) * (1.0f / 255.0f);
+		col[1] = float(srgb_g) * (1.0f / 255.0f);
+		col[2] = float(srgb_b) * (1.0f / 255.0f);
+	
+		sRGB2sRGBL(col, col); // sRGB to Linear sRGB
+		sRGBL2LEDL(col, col); // Linear sRGB -> XYZ -> Linear LED RGB
+	
+		for (size_t c = 0; c < 3; c++) {
+			if (col[c] < 0.0f) {
+				col[c] = 0.0f;
+			}
+			if (col[c] > 1.0f) {
+				col[c] = 1.0f;
+			}
+		}
+	
+		pwm_r = uint16_t(col[0]*float(pwm_l));
+		pwm_g = uint16_t(col[1]*float(pwm_l));
+		pwm_b = uint16_t(col[2]*float(pwm_l));
+#endif            
+	}
+
 private:
+
+	inline int32_t mul_fixed(int32_t x, int32_t y) const {
+    	return int32_t((int64_t(x) * int64_t(y)) >> fixed_shift);
+	}
 
     float srgbl2ledl[9];
     float ledl2srgbl[9];
@@ -102,7 +149,6 @@ private:
                             float *rgb2xyz,
                             float *xyz2rgb) const;
 };
-
 
 class rgbww {
 public:
