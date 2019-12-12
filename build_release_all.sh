2f2099f41b72faf36992d879c44169c91c071b8e
Tinic Uro <turo@adobe.com>
2019-12-12 11:14:55 -0800

    Enforce setting of BOOTLOADED or BOOTLOADER

--- build_release_all.sh
+++ build_release_all.sh
@@ -1,8 +1,6 @@
 #/bin/sh
 mkdir -p build
 cd build
-cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release ..
-ninja
 cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADED=1 ..
 ninja
 cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DBOOTLOADER=1 ..
