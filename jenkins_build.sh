d66172b471580adf050f38701d84bf7b6886d3a8
tinic <tinicuro@gmail.com>
2024-12-25 02:26:55 -0800

    Update jenkins_build.sh

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -17,14 +17,14 @@ arm-none-eabi-objdump > build/lightkraken_bootloaded.dump.txt -D build/lightkrak
 arm-none-eabi-objdump > build/lightkraken_bootloader.annotated.txt -S build/lightkraken_bootloader.elf 
 arm-none-eabi-objdump > build/lightkraken_bootloaded.annotated.txt -S build/lightkraken_bootloaded.elf 
 
-buildnumber=$(git rev-list HEAD --count)
+#buildnumber=$(git rev-list HEAD --count)
 
-zip -j build.zip build/lightkraken_bootloader.bin build/lightkraken_bootloaded.bin
-github-release delete --tag latest || true
-git remote rm origin
-git remote add origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/tinic/lightkraken.git
-git push --delete origin latest || true
-github-release release --tag latest --name "latest"
+#zip -j build.zip build/lightkraken_bootloader.bin build/lightkraken_bootloaded.bin
+#github-release delete --tag latest || true
+#git remote rm origin
+#git remote add origin https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/tinic/lightkraken.git
+#git push --delete origin latest || true
+#github-release release --tag latest --name "latest"
 #fix race on github.com
-sleep 5
-github-release upload --tag latest --name "lightkraken-bin-$(date +"%Y-%m-%d")-r${buildnumber}.zip" --file build.zip
+#sleep 5
+#github-release upload --tag latest --name "lightkraken-bin-$(date +"%Y-%m-%d")-r${buildnumber}.zip" --file build.zip
